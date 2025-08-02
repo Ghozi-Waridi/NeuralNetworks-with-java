c61d3b3a900544c0df61db191189c4f1769e1350
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-08-02 16:01:33 +0700

    :fire: REMOVE: hapus File TensorOperation.java

--- src/com/neuralnetworks/math/Tensor.java
+++ src/com/neuralnetworks/math/Tensor.java
@@ -1,12 +1,56 @@
 package com.neuralnetworks.math;
 
-abstract class Tensor<T> {
-    /**
+import java.util.ArrayList;
+
+
+/**
+ * Tensor
+ *
+ * class Tensor yang menyediakan struktur data sebagai tensor untuk penyimpanan data 
+ * dan operasi matematika pada neural networks.
+ */
+public class Tensor<T extends Number>{
+  /**
    * @field shape menyimpan dimensi dari tensor
    * @field data menyimpan data yang disimpan dalam tensor
    */
 
-  protected int[] shape;
-  protected T data;
+  private int[] shape;
+  private T data;
+  
+    /**
+   * Kontruktor untuk membuat tensor denagn dimensi tertentu
+   * @param shape dimensi dari tensor
+   * @param data yang di jadikan sebagia struktur data tensor
+   */
+
+  public Tensor(int[] shape, T data) {
+    this.shape = shape;
+    this.data = data;
+  }
+  
+  /**
+   * getShape Mengambil dimensi dari tensor
+   * @return dimensi dari tensor (nilai instance variabel dari shape)
+   */
+
+  public int[] getShape() {
+    return this.shape;
+  }
+
+  
+
+  public Tensor<T> fromArray(T array){
+    return null;
+  };
+  public Tensor<T> addF(Tensor<T>[] A) {
+    return null;
+  };
+  public Tensor<T> addB(Tensor<T>[] A){
+    return null;
+  };
+  public Tensor<T> copy(Tensor<T> A){
+    return null;
+  };
   
 }
--- src/com/neuralnetworks/math/TensorOperationMath.java
+++
@@ -1,7 +0,0 @@
-package com.neuralnetworks.math;
-
-public class Operation {
-  public static void dot(){
-    System.out.println("Perkalian Matriks");
-  }
-}
