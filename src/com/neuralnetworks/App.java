3503e0c9c671ad8b9e73aa346a6de8622ed253e7
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-08-03 20:54:04 +0700

    :sparkles: FEATURE: Create subctact dan mutlipy

--- src/com/neuralnetworks/App.java
+++ src/com/neuralnetworks/App.java
@@ -7,6 +7,7 @@ public class App {
   public static void main(String[] args) {
 
     double[] data = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
+    double[] data3 = { 10.0, 20.0, 30.0, 40.0, 50.0, 60.0 };
     double[] data2 = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
     Tensor<Double> tensor = Tensor.fromArray(data, 2, 3);
     Tensor<Double> tensor2 = Tensor.fromArray(data2, 2, 3);
@@ -14,5 +15,12 @@ public class App {
     Tensor<Double> hasilPenjumlahan = TensorMath.add(tensor, tensor2);
 
     System.out.println("Hasil Penjumlahan Tensor:" + hasilPenjumlahan);
+
+    Tensor<Double> tensor3 = Tensor.fromArray(data3, 2, 3);
+    Tensor<Double> hasilPengurangan = TensorMath.subtract(tensor3, tensor);
+    System.out.println("Hasil Pengurangan Tensor: " + hasilPengurangan);
+
+    Tensor<Double> hasilPerkalian = TensorMath.multiply(tensor, tensor2);
+    System.out.println("Hasil Perkalian Tensor: " + hasilPerkalian);
   }
 }
--- src/com/neuralnetworks/math/TensorMath.java
+++ src/com/neuralnetworks/math/TensorMath.java
@@ -15,6 +15,8 @@ public class TensorMath<T extends Number> {
    * @param b Tensor kedua yang akan dijumlahkan.
    * @return Tensor baru yang merupakan hasil penjumlahan dari a dan b.
    * @param T Tipe data numerik
+   *
+   *
    */
   @SuppressWarnings("unchecked")
   public static <T extends Number> Tensor<T> add(Tensor<T> a, Tensor<T> b) {
@@ -45,4 +47,82 @@ public class TensorMath<T extends Number> {
     return result;
   }
 
+  /**
+   * Melakukan operasi pengurangan antara dua matriks atau tensor,
+   * Perlu diketahui bahwa operasi ini hanya mendukung tipe data Double.
+   *
+   * @param a Tensor pertama yang akan dikurangi.
+   * @param b Tensor kedua yang akan dikurangi.
+   * @return Tensor baru yang merupakan hasil pengurangan dari a dan b.
+   * @param T Tipe data numerik (Double)
+   */
+  @SuppressWarnings("unchecked")
+  public static <T extends Number> Tensor<T> subtract(Tensor<T> a, Tensor<T> b) {
+    // validasi dimensi Tensor
+    if (a.getShapeArray().length != b.getShapeArray().length || a.getShapeArray()[0] != b.getShapeArray()[0]
+        || a.getShapeArray()[1] != b.getShapeArray()[1]) {
+      throw new IllegalArgumentException("Tensor harus memiliki dimensi yang sama untuk pengurangan.");
+    }
+
+    // Membuat tensor hasil dengan shape yang sama dengan tensor a atau tensor b.
+    Tensor<T> result = Tensor.create(a.getShapeArray());
+
+    // Mengambil data internal dari tensor a dan b
+    List<T> dataA = a.getInternalData();
+    List<T> dataB = b.getInternalData();
+
+    // Membuat list untuk menyimpan hasil pengurangan
+    List<T> hasil = new ArrayList<>(dataA.size());
+
+    // Melakukan pengurangan elemen demi elemen
+    for (int i = 0; i < dataA.size(); i++) {
+      T valueA = dataA.get(i);
+      T valueB = dataB.get(i);
+      double substraction = valueA.doubleValue() - valueB.doubleValue();
+      hasil.add((T) Double.valueOf(substraction));
+    }
+    // Merubah nilai result dengan hasil pengurangan
+    result.set(hasil);
+    return result;
+  }
+
+  /**
+   * Melakukan operasi perkalian antara dua matriks atau tensor,
+   * Perlu diketahui bahwa operasi ini hanya mendukung tipe data Double.
+   *
+   * @param a Tensor pertama yang akan dikalikan.
+   * @param b Tensor kedua yang akan dikalikan.
+   * @return Tensor baru yang merupakan hasil perkalian dari a dan b.
+   * @param T Tipe data numerik (Double)
+   *
+   *
+   */
+
+  @SuppressWarnings("unchecked")
+  public static <T extends Number> Tensor<T> multiply(Tensor<T> a, Tensor<T> b) {
+    // validasi dimensi Tensor
+    if (a.getShapeArray().length != b.getShapeArray().length || a.getShapeArray()[0] != b.getShapeArray()[0]
+        || a.getShapeArray()[1] != b.getShapeArray()[1]) {
+      throw new IllegalArgumentException("Tensor harus memiliki dimensi yang sama untuk pengurangan.");
+    }
+
+    // Membuat tensor hasil dengan shape yang sama dengan tensor a atau tensor b.
+    Tensor<T> result = Tensor.create(a.getShapeArray());
+
+    // Mengambil data internal dari tensor a dan b
+    List<T> dataA = a.getInternalData();
+    List<T> dataB = b.getInternalData();
+
+    // Membuat list untuk menyimpan hasil perkalian
+    List<T> hasil = new ArrayList<>(dataA.size());
+
+    for (int i = 0; i < dataA.size(); i++) {
+      T valueA = dataA.get(i);
+      T valueB = dataB.get(i);
+      double multiplication = valueA.doubleValue() * valueB.doubleValue();
+      hasil.add((T) Double.valueOf(multiplication));
+    }
+    result.set(hasil);
+    return result;
+  }
 }
