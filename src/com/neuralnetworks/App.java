2c1e00998606265b133a1688e08bddb5b73ffe86
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-08-18 20:19:05 +0700

    :star: ADD: Create NN

--- src/com/neuralnetworks/App.java
+++ src/com/neuralnetworks/App.java
@@ -11,24 +11,22 @@ import com.neuralnetworks.data.ToTensor;
 
 import java.util.List;
 
-
 public class App {
   public static void main(String[] args) {
-    List<String> labels = List.of("no", "yes");
-    String PATH = "data/raw";
-    ImageFolderDataset dataset = new ImageFolderDataset(PATH);    
-
-    // System.out.println(dataset.get(2).toString());  
-    List<Transform> pipelines = List.of(
-      new GrayScale(),
-      new Resize(28, 28),
-      new ToTensor(labels)
-    );
-
-    DataLoader loader = new DataLoader(dataset, pipelines);
-    Object data = loader.prosesData(100);
-    System.out.println(data);
-
+    // List<String> labels = List.of("no", "yes");
+    // String PATH = "data/raw";
+    // ImageFolderDataset dataset = new ImageFolderDataset(PATH);
+    //
+    // // System.out.println(dataset.get(2).toString());
+    // List<Transform> pipelines = List.of(
+    // new GrayScale(),
+    // new Resize(28, 28),
+    // new ToTensor(labels)
+    // );
+    //
+    // DataLoader loader = new DataLoader(dataset, pipelines);
+    // Object data = loader.prosesData(100);
+    // System.out.println(data);
 
   }
 }
