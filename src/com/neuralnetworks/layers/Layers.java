6053158efc6c223562ebf2a54c0c14ac57ac8be3
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-08-18 16:19:37 +0700

    :star: ADD: Feature Layers
    menambahkan abstract class untuk file layers

---
+++ src/com/neuralnetworks/layers/Layers.java
@@ -0,0 +1,16 @@
+package com.neuralnetworks.layers;
+
+import com.neuralnetworks.math.Tensor;
+
+/**
+ * InnerLayers
+ */
+public abstract class Layers {
+
+  protected Tensor input;
+  protected Tensor output;
+
+  public abstract Tensor forward(Tensor input);
+
+  public abstract Tensor backward(Tensor outputGradient);
+} 
